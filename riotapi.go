5c2e1cc0dc80a89af05fd3c1280fc2b2aa127620
YoureWaifuABest <slgd@airmail.cc>
2017-08-19 17:44:44 -0600

    Begin adding basic data caching for riot api

--- riotapi.go
+++ riotapi.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"strings"
+	"time"
 )
 
 type item struct {
@@ -77,26 +79,72 @@ func parseData(itemID string, data []byte) (item item) {
 }
 
 func getValue(url string) ([]byte, error) {
-	r, err := http.NewRequest("GET", url, nil)
+	var modify bool
+	/*
+	 * Very basic caching, probably not very efficient
+	 * Hard coded file for now, since I'm not sure what I'm going to do with directories
+	 * This also assumes the "data" directory is already created,
+	 * rather than creating the directory if it's not
+	 *
+	 * Obviously that's an issue which needs to be fixed at some point
+	 */
+	info, err := os.Stat("data/items")
+	/*
+	 * If os.Stat claims the file does not exist, set modify to true
+	 * Meaning the file needs to be modified (in this case, created)
+	 */
+	if os.IsNotExist(err) {
+		modify = true
+	} else if checkErrorPrint(err) {
+		return nil, err
+	} else if (time.Now().Sub(info.ModTime())) > (30 * time.Minute) {
+		/*
+		 * time.Now() returns the current time
+		 * info.ModTime() returns when the file from os.Stat was last modified
+		 *
+		 * time.Now() - info.ModTime() = Time elapsed since file has been modified
+		 * If this value is greater than 30 * time.Minute (30 minutes),
+		 * set modify to true
+		 */
+		modify = true
+	}
+
+	if modify == true {
+		fmt.Println("Changed!")
+		r, err := http.NewRequest("GET", url, nil)
+		if checkErrorPrint(err) {
+			return nil, err
+		}
+
+		r.Header.Add("X-Riot-Token", rtoken)
+		resp, err := client.Do(r)
+		if checkErrorPrint(err) {
+			return nil, err
+		}
+
+		if resp.StatusCode != 200 {
+			fmt.Println(resp.StatusCode)
+			return nil, fmt.Errorf("response code != 200, code: %d", resp.StatusCode)
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if checkErrorPrint(err) {
+			return nil, err
+		}
+
+		/* Dumps body into file */
+		err = ioutil.WriteFile("data/items", body, 0644)
+		if checkErrorPrint(err) {
+			return nil, err
+		}
+
+		return body, nil
+	}
+	fmt.Println("Did not change!")
+	/* Spit the whole file out */
+	fileContents, err := ioutil.ReadFile("data/items")
 	if checkErrorPrint(err) {
-		return
+		return nil, err
 	}
-
-	r.Header.Add("X-Riot-Token", rtoken)
-	resp, err := client.Do(r)
-	if checkErrorPrint(err) {
-		return
-	}
-
-	if resp.StatusCode != 200 {
-		fmt.Println(resp.StatusCode)
-		return nil, fmt.Errorf("response code != 200, code: %d", resp.StatusCode)
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	return body, nil
+	return fileContents, nil
 }
